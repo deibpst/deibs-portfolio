ed306ba2cf428e52025f9eadddbb613193bf45e5
deibfps <id1386859@gmail.com>
2025-11-20 17:28:24 -0600

    Added svg for switch, js scripts for theme, navbar in HTML and styles in css files

---
+++ theme.js
@@ -0,0 +1,13 @@
+const switchTheme = () => {
+    const rootElem = document.documentElement
+    let dataTheme = rootElem.getAttribute('data-theme'),
+        newTheme
+
+    newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+    rootElem.setAttribute('data-theme', newTheme)
+
+    localStorage.setItem('theme', newTheme)
+}
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
